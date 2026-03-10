2d75a2beb4f406f1fbbfab464205a7dadd159031
emandiem <diem25jn@student.ju.se>
2026-03-10 19:45:44 +0100

    Created a functional cart page

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 const buttonelement = document.getElementById("add-to-cart");
+const emptyCart = document.getElementById("empty-cart");
 
-buttonelement.addEventListener("click", function () {
-  let product = document.querySelector(
-    ".product-page-description h1",
-  ).textContent;
-  localStorage.product = product;
-  let newProduct = localStorage.product;
-  alert(`${newProduct} has been added to your cart!`);
-});
+if (buttonelement) {
+  buttonelement.addEventListener("click", function () {
+    // makes the button clickable //
+    const product = document.querySelector(
+      ".product-page-description h1",
+    ).textContent; // gets the product name from the h1 element //
+
+    localStorage.product = product; // stores the product name in local storage //
+    alert(`${product} has been added to your cart!`);
+  });
+}
+
+if (localStorage.product) {
+  emptyCart.innerText = localStorage.product;
+}
